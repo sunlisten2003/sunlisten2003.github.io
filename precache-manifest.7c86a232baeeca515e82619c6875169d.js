df9b11eda58e9acbcbf30512e58504234627f833
sunlisten2003 <466781879@qq.com>
2019-12-25 15:23:24 +0800

    shopincat frontend

---
+++ precache-manifest.7c86a232baeeca515e82619c6875169d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56afe71cfc253978fa6010380706b68e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff099226ff7db7d1edb",
+    "url": "/static/css/2.8b82153e.chunk.css"
+  },
+  {
+    "revision": "3488106240b97cc9b1eb",
+    "url": "/static/css/main.21d0a281.chunk.css"
+  },
+  {
+    "revision": "fff099226ff7db7d1edb",
+    "url": "/static/js/2.20fff9ad.chunk.js"
+  },
+  {
+    "revision": "ff0fa456f1cb57c94eb54d3d6ecfb814",
+    "url": "/static/js/2.20fff9ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3488106240b97cc9b1eb",
+    "url": "/static/js/main.269758ce.chunk.js"
+  },
+  {
+    "revision": "46cac47029644a56bfb6",
+    "url": "/static/js/runtime-main.e8a3c38e.js"
+  }
+]);
